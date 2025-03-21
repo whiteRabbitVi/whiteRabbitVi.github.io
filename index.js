9e2918c6c65450d5ff7ea55878c7e3801fb2d4c8
white_rabbit <34686941+whiteRabbitVi@users.noreply.github.com>
2025-03-21 19:22:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // index.js
-function setDate(date) {
-  const nameWrapper = document.querySelector('.js-date');
+function HelloWorld(props) {
+  const computedDate = props.date ? props.date : '29.05.2013';
 
-  if (nameWrapper) {
-    nameWrapper.textContent = date || '29.05.2013';
-  }
+  return (<h1>Привет, Мир!!! Моё приложение создано: <b>{computedDate}</b></h1>)
 }
 
-setDate('13.02.23');
+const root = createRoot(document.getElementById("root"));
+
+root.render(HelloWorld({date: '13.02.23'}));
